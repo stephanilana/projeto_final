336eefb8b4df83c11eaf405ee4f173f1c7b4a027
thaylaalves <thaylavitoriar@gmail.com>
2024-10-29 19:44:46 -0300

    ajustando

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,24 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
-import bodyParser from 'body-parser'; 
-import alunosRouter from './routes/alunos.routes'; 
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import bodyParser from 'body-parser'
+import alunosRouter from './routes/alunos.routes'
+import turmasRouter from './routes/turmas.routes'
 
-dotenv.config();
+dotenv.config()
 
-const app = express();
+const app = express()
 
 // Configurações do middleware
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json({ type: 'application/vnd.api+json' }));
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-app.use(cookieParser());
-app.use(cors());
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json({ type: 'application/vnd.api+json' }))
+app.use(bodyParser.json({ limit: '50mb' }))
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }))
+app.use(cookieParser())
+app.use(cors())
 
 // Roteamentos
-app.use(alunosRouter);
-export default app;
+app.use(alunosRouter)
+app.use(turmasRouter)
+export default app
