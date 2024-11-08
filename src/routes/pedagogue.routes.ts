b0a92a861363bcbdd3e1a478fab5a1db06a6f08a
JoaoPellizzoni <joaovitorpellizzonitrotzki@gmail.com>
2024-11-07 22:24:37 -0300

    Criação do pedagogo

---
+++ src/routes/pedagogue.routes.ts
@@ -0,0 +1,8 @@
+import Router from 'express-promise-router';
+import pedagogosController from '../controllers/pedagogue.controller';
+ 
+const router = Router();
+ 
+router.post('/pedagogos', pedagogosController.createPedagogue);
+ 
+export default router;
