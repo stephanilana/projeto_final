65634972906abdc61c6bcac5df4149141c301d41
enzo-savaris <savarisenzo2@gmail.com>
2024-11-11 21:20:04 -0300

    arrumando erros de códigos

--- src/services/teacher.service.ts
+++ src/services/teacher.service.ts
@@ -67,11 +67,11 @@ async function updateTeacher(
       resposta = "ID, Nome e CPF são obrigatórios.";
       return resposta;
     }
-    resposta = `O aluno que atualizamos é ${nome} que possui o CPF ${cpf}`;
+    resposta = `O professor que atualizamos é ${nome} que possui o CPF ${cpf}`;
     return resposta;
   } catch (error) {
-    console.error("Erro ao atualizar aluno:", error);
-    return "Erro ao atualizar aluno";
+    console.error("Erro ao atualizar professor:", error);
+    return "Erro ao atualizar professor";
   }
 }
 
