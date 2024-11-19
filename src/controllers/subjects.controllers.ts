525f663f049c3179312def2ea709fd756825a50c
Ariel15012008 <arielcristiandemenezes4@gmail.com>
2024-11-18 22:14:21 -0300

    fiz adicionando no banco

--- src/controllers/subjects.controllers.ts
+++ src/controllers/subjects.controllers.ts
@@ -1,16 +1,26 @@
 import { Request, Response } from 'express';
 import { materiaService } from '../services/subjects.service';
 
-const materiasController = {
-    createMateria: async (req: Request, res: Response): Promise<void> => {
-        const { nomeMatéria, cargaHorária, dataInício, dataFim, idProfessor, id } = req.body;
+const subjectsController = {
+    createSubject: async (req: Request, res: Response): Promise<void> => {
+        const { idCurso, nomeMatéria, cargaHorária, dataInício, dataFim, idProfessor, id: idMateria, ementa } = req.body;
 
-        if (!nomeMatéria || !cargaHorária || !dataInício || !dataFim || !idProfessor || !id) {
+        if (!idCurso || !nomeMatéria || !cargaHorária || !dataInício || !dataFim || !idProfessor || !idMateria || !ementa) {
             res.status(400).send('Todos os campos são obrigatórios.');
             return;
         }
+
         try {
-            const retorno = await materiaService.createMateria(nomeMatéria, cargaHorária, dataInício, dataFim, idProfessor, id);
+            const retorno = await materiaService.createSubject(
+                idMateria,
+                nomeMatéria,
+                cargaHorária,
+                dataInício,
+                dataFim,
+                idProfessor,
+                ementa,
+                idCurso
+            );
             res.status(200).send(retorno);
         } catch (error) {
             console.error('Erro ao cadastrar matéria:', error);
@@ -18,11 +28,28 @@ const materiasController = {
         }
     },
 
-    updateMateria: async (req: Request, res: Response): Promise<void> => {
-        const { nomeMatéria, cargaHorária, dataInício, dataFim, idProfessor } = req.body;
-        const id = req.params.id;
+    updateSubject: async (req: Request, res: Response): Promise<void> => {
+        const { idMateria, idCurso, idProfessor } = req.params;
+    
+        const { nomeMatéria, cargaHorária, dataInício, dataFim, ementa } = req.body;
+    
+        if (!idMateria || !idCurso || !idProfessor || !nomeMatéria || !cargaHorária || !dataInício || !dataFim || !ementa) {
+            res.status(400).send('Todos os campos são obrigatórios.');
+            return;
+        }
+    
         try {
-            const ret = await materiaService.updateMateria(id, nomeMatéria, cargaHorária, dataInício, dataFim, idProfessor);
+            const ret = await materiaService.updateSubject(
+                idMateria,
+                idCurso,
+                nomeMatéria,
+                cargaHorária,
+                dataInício,
+                dataFim,
+                idProfessor,
+                ementa,
+            );
+    
             if (!ret) {
                 res.status(500).send('Não foi possível atualizar a matéria.');
             } else {
@@ -33,17 +60,21 @@ const materiasController = {
             res.status(500).send('Ocorreu um erro no servidor ao tentar atualizar a matéria.');
         }
     },
+    
+    deleteSubject: async (req: Request, res: Response): Promise<void> => {
+        const idMateria = req.params.id;
 
-    deleteMateria: async (req: Request, res: Response): Promise<void> => {
-        const id = req.params.id;
-        const { nomeMatéria } = req.body;
+        if (!idMateria) {
+            res.status(400).send('ID da matéria é obrigatório.');
+            return;
+        }
 
         try {
-            const ret = await materiaService.deleteMateria(id, nomeMatéria);
+            const ret = await materiaService.deleteSubject(idMateria); 
             if (!ret) {
                 res.status(500).send('Não foi possível deletar a matéria.');
             } else {
-                res.status(200).send(ret);
+                res.status(200).send('Matéria deletada com sucesso.');
             }
         } catch (error) {
             console.error('Erro ao deletar matéria:', error);
@@ -51,15 +82,16 @@ const materiasController = {
         }
     },
 
-    getMateriaById: async (req: Request, res: Response): Promise<void> => {
-        const id = req.params.id;
+    getSubjectById: async (req: Request, res: Response): Promise<void> => {
+        const idMateria = req.params.id;
 
-        if (!id) {
+        if (!idMateria) {
             res.status(400).send('ID da matéria é obrigatório.');
             return;
         }
+
         try {
-            const ret = await materiaService.getMateriaById(id);
+            const ret = await materiaService.getSubjectById(idMateria);
             if (!ret) {
                 res.status(404).send('Matéria não encontrada.');
             } else {
@@ -72,4 +104,4 @@ const materiasController = {
     }
 };
 
-export default materiasController;
+export default subjectsController;
