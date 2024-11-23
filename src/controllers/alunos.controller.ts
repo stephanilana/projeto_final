d5ae1ee1247c94732cc4eb7635ca1f3f34fbd4eb
Beatriz Defrein <beatrizdefrein67@gmail.com>
2024-11-23 14:21:30 -0300

    trying to fix error when creating student

--- src/controllers/alunos.controller.ts
+++ src/controllers/alunos.controller.ts
@@ -3,13 +3,44 @@ import { alunoService } from '../services/alunos.service';
 
 const alunosController = {
     createAlunos: async (req: Request, res: Response): Promise<void> => {
-        const {id, nomeCompleto, cpf, email, telefone, estado, municipio, rua, bairro, numeroDaCasa, dataDeNascimento, rg, dataExpedicaoRg, estadoDeNascimento, cidadeDeNascimeto} = req.body;
+        const {id,
+            nomeCompleto,
+            email,
+            estadodeexpedicaorg,
+            estado,
+            municipio,
+            rua,
+            bairro, 
+            numero,
+            dataDeNascimento, 
+            cpf,
+            rg,
+            dataExpedicaoRg, 
+            estadoDeNascimento,
+            cidadeDeNascimeto} = req.body;
+            console.log(req.body);
+            
+        
         try {
-            const retorno = await alunoService.createAluno(id, nomeCompleto, cpf, email, telefone, estado, municipio, rua, bairro, numeroDaCasa, dataDeNascimento, rg, dataExpedicaoRg, estadoDeNascimento, cidadeDeNascimeto);
+            const retorno = await alunoService.createAluno(id,
+                nomeCompleto,
+                email,
+                estadodeexpedicaorg,
+                estado,
+                municipio,
+                rua,
+                bairro, 
+                numero,
+                dataDeNascimento, 
+                cpf,
+                rg,
+                dataExpedicaoRg, 
+                estadoDeNascimento,
+                cidadeDeNascimeto);
             if (!retorno) {
                 res.status(500).send('Não foi possível cadastrar o aluno.');
             } else {
-                res.status(200).send('Cadastro realizado com sucesso');
+                res.status(200).send(retorno);
             }
         } catch (error) {
             console.error('Erro ao cadastrar aluno:', error);
@@ -18,10 +49,38 @@ const alunosController = {
     },
 
     updateAlunos: async (req: Request, res: Response): Promise<void> => {
-        const {nomeCompleto, cpf, email, telefone, estado, municipio, rua, bairro, numeroDaCasa, dataDeNascimento, rg, dataExpedicaoRg, estadoDeNascimento, cidadeDeNascimeto } = req.body;
+        const {nomeCompleto,
+            email,
+            telefone,
+            estado,
+            municipio,
+            rua,
+            bairro, 
+            numero,
+            dataDeNascimento, 
+            cpf,
+            rg,
+            dataExpedicaoRg, 
+            estadoDeNascimento,
+            cidadeDeNascimeto} = req.body;
         const id = req.params.id; 
         try {
-            const ret = await alunoService.updateAluno(id, nomeCompleto, cpf, email, telefone, estado, municipio, rua, bairro, numeroDaCasa, dataDeNascimento, rg, dataExpedicaoRg, estadoDeNascimento, cidadeDeNascimeto);
+            const ret = await alunoService.updateAluno(
+                id,
+                nomeCompleto,
+                email,
+                telefone,
+                estado,
+                municipio,
+                rua,
+                bairro, 
+                numero,
+                dataDeNascimento, 
+                cpf,
+                rg,
+                dataExpedicaoRg, 
+                estadoDeNascimento,
+                cidadeDeNascimeto);
             if (!ret) {
                 res.status(500).send('Não foi possível atualizar o aluno.');
             } else {
@@ -55,7 +114,7 @@ const alunosController = {
             if (!ret) {
                 res.status(500).send('Não foi possível deletar o aluno.');
             } else {
-                res.status(200).send('Aluno delatado com sucesso');
+                res.status(200).send(ret);
             }
         } catch (error) {
             console.error('Erro ao deltar o aluno:', error);
--- src/services/alunos.service.ts
+++ src/services/alunos.service.ts
@@ -1,36 +1,49 @@
 import {db} from '../config/database';
 
-async function createAluno(id: string, nomeCompleto: string, email: string, telefone: string, estado: string, municipio: string, rua: string, bairro: string, numeroDaCasa: number, dataDeNascimento: string, cpf: string, rg: string, dataExpedicaoRg: string, estadoDeNascimento: string, cidadeDeNascimeto: string): Promise<string> {
+async function createAluno(id: string, nomeCompleto: string, email: string, estadodeexpedicaorg: string, estado: string, municipio: string, rua: string, bairro: number, numero: number, dataDeNascimento: Date, cpf: string, rg: string, dataExpedicaoRg: Date, estadoDeNascimento: string, cidadeDeNascimeto: string,): Promise<string> {
     try {
         let resposta = "";
-        if (!id || !nomeCompleto || !email || !telefone || !estado || !municipio || !rua || !bairro || !numeroDaCasa || !dataDeNascimento || !cpf || !rg || !dataExpedicaoRg || !estadoDeNascimento || !cidadeDeNascimeto) {
+        if (!id 
+            || !nomeCompleto 
+            || !email 
+            || !estado 
+            || !municipio 
+            || !rua 
+            || !bairro 
+            || !numero
+            || !dataDeNascimento 
+            || !cpf 
+            || !rg 
+            || !dataExpedicaoRg 
+            || !estadoDeNascimento
+            || !estadodeexpedicaorg
+            || !cidadeDeNascimeto) {
             resposta = 'Todos os campos são obrigatórios para cadastrar o aluno.';
             return resposta;
         }
 
-        const response = await db.query(
-            "INSERT INTO aluno (id_aluno, nome, data_nasc, email, estado, municipio, rua, bairro, numero, rg, datadeexpedicaorg, estadodeexpedicaorg, estadonascimento, cidadenascimento, cpf) VALUES ($1, $2, $3, $4, $5, $6, $7, $7, $8, $9, $10, $11, $12, $13, $14, $15)",
+        await db.query(
+            "INSERT INTO alunos (id_aluno, nome, data_nasc, email, estado, municipio, rua, bairro, numero, rg, datadeexpedicaorg, estadodeexpedicaorg, estadonascimento, cidadenascimento, cpf) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15)",
             [
                 id,
                 nomeCompleto,
+                dataDeNascimento, 
                 email,
-                telefone,
                 estado,
                 municipio,
                 rua,
-                bairro, 
-                numeroDaCasa,
-                dataDeNascimento,
-                cpf,
-                rg,
-                dataExpedicaoRg, 
+                bairro,
+                numero,
+                rg, 
+                dataExpedicaoRg,
+                estadodeexpedicaorg,
                 estadoDeNascimento,
-                cidadeDeNascimeto
-
+                cidadeDeNascimeto,
+                cpf,
             ]
             );
-            resposta = `O aluno cadastrado foi ${nomeCompleto} com o cpf ${cpf}´`;
-             return response.rows[0], resposta;
+            resposta = await getStudent(id);
+            return resposta;
         
     } catch (error) {
         console.error('Erro ao criar aluno:', error);
@@ -39,14 +52,13 @@ async function createAluno(id: string, nomeCompleto: string, email: string, tele
 }
 async function updateAluno(id: string, nomeCompleto: string, cpf: string, email: string, telefone: string, estado: string, municipio: string, rua: string, bairro: string, numeroDaCasa: number, dataDeNascimento: string, rg: string, dataExpedicaoRg: string, estadoDeNascimento: string, cidadeDeNascimeto: string): Promise<string> {
     try {
-        // const professor = db.query('insert'); // Substitua por sua query real de inserção
         let resposta = "";
-        if (!nomeCompleto || !cpf || !email || !telefone || !estado || !municipio || !rua || !bairro || !numeroDaCasa || !dataDeNascimento || !rg || !estadoDeNascimento || !cidadeDeNascimeto || !dataExpedicaoRg) {
+        if (!id || !nomeCompleto || !cpf || !email || !telefone || !estado || !municipio || !rua || !bairro || !numeroDaCasa || !dataDeNascimento || !rg || !estadoDeNascimento || !cidadeDeNascimeto || !dataExpedicaoRg) {
             resposta = 'Todos os campos são obrigatórios para cadastrar o aluno.';
             return resposta;
         }
-        const response = await db.query(
-            "INSERT INTO aluno (id_aluno, nome, data_nasc, email, estado, municipio, rua, bairro, numero, rg, datadeexpedicaorg, estadodeexpedicaorg, estadonascimento, cidadenascimento, cpf) VALUES ($1, $2, $3, $4, $5, $6, $7, $7, $8, $9, $10, $11, $12, $13, $14, $15)",
+        await db.query(
+            "INSERT INTO alunos (id_aluno, nome, data_nasc, email, estado, municipio, rua, bairro, numero, rg, datadeexpedicaorg, estadodeexpedicaorg, estadonascimento, cidadenascimento, cpf) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15)",
             [
                 id,
                 nomeCompleto,
@@ -57,7 +69,7 @@ async function updateAluno(id: string, nomeCompleto: string, cpf: string, email:
                 rua,
                 bairro, 
                 numeroDaCasa,
-                dataDeNascimento,
+                dataDeNascimento, 
                 cpf,
                 rg,
                 dataExpedicaoRg, 
@@ -66,8 +78,8 @@ async function updateAluno(id: string, nomeCompleto: string, cpf: string, email:
 
                 ]
             );
-            resposta = "O nome do aluno cadastrado foi ${nomeCompleto} com o cpf ${cpf}";
-            return response.rows[0], resposta;
+            resposta = await getStudent(id);
+            return resposta;
 
     } catch (error) {
         console.error('Erro ao atualizar aluno:', error);
@@ -77,7 +89,6 @@ async function updateAluno(id: string, nomeCompleto: string, cpf: string, email:
 
 async function deleteStudent(id: string): Promise<string> {
     try {
-        // const professor = db.query('insert'); // Substitua por sua query real de inserção
         let resposta = '';
         if (!id){
             resposta = 'O id é obrigatório para deletar o aluno.';
@@ -106,13 +117,12 @@ async function getStudent(id: string): Promise<string> {
             return resposta;
         }
         const response = await db.query(
-            "INSERT INTO aluno (id_aluno) VALUES ($1)",
+            "select * from alunos where id_aluno = $1",
             [
-                id,
+                parseInt(id),
                 ]
             );
-            resposta = `O aluno que buscamos possuí o id${id}` ;
-            return response.rows[0], resposta;
+            return response.rows[0];
 
     } catch (error) {
         console.error('Erro ao buscar o aluno:', error);
@@ -121,18 +131,22 @@ async function getStudent(id: string): Promise<string> {
 }
 
 export const alunoService = {
-    createAluno: (id: string, nomeCompleto: string, email: string, telefone: string,   
-        estado: string,
-        municipio: string,
-        rua: string,
-        bairro: string,
-        numeroDaCasa: number, 
-        dataDeNascimento: string, 
-        cpf: string,
+    createAluno: (id : string,
+        nomeCompleto: string, 
+        email: string,
+        estadodeexpedicaorg: string, 
+        estado: string, 
+        municipio: string, 
+        rua: string, 
+        bairro: number, 
+        numero: number, 
+        dataDeNascimento: Date, 
+        cpf: string, 
         rg: string, 
-        dataExpedicaoRg: string, 
+        dataExpedicaoRg: Date, 
         estadoDeNascimento: string, 
-        cidadeDeNascimeto: string) => createAluno(id, nomeCompleto, email, telefone, estado, municipio, rua, bairro, numeroDaCasa, dataDeNascimento, cpf, rg, dataExpedicaoRg, estadoDeNascimento, cidadeDeNascimeto),
+        cidadeDeNascimeto: string,
+        ) => createAluno(id, nomeCompleto, email, estadodeexpedicaorg, estado, municipio, rua, bairro, numero, dataDeNascimento, cpf,rg, dataExpedicaoRg, estadoDeNascimento, cidadeDeNascimeto),
     updateAluno: (id: string,
         nomeCompleto: string, 
         cpf: string, 
